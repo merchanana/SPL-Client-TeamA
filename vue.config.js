a194d8308f3850df4a463c7f5e9fe6138ec3f10c
mariamerchan <alejandramerchan25@gmail.com>
2023-08-08 20:59:15 -0500

    Add configuration to allow all hosts

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  transpileDependencies: ["vuetify"],
+  publicPath: "./",
+  devServer: {
+    allowedHosts: "all",
+  },
+});
